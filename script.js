1f749f6eb1a9e33e666c996dae9d6faad2d47d55
eternaluser18 <80708910+eternaluser18@users.noreply.github.com>
2023-11-01 14:53:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function func() {
-    alert('Hellow');
-    document.getElementsByClassName('omg').style = "color: red";
+    var ok = window.getComputedStyle(document.getElementById("omg"), null).display;
+    if(ok === "block") {
+    document.getElementById("omg").style.display = "none"; 
+    }
+    if(ok === "none") {
+    document.getElementById("omg").style.display = "block"; 
+    }
+    alert(ok)
 }
