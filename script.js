265bd543b29d2186c025bd1f80e21c1fb39c9478
eternaluser18 <80708910+eternaluser18@users.noreply.github.com>
2024-01-19 18:56:32 +0100

    minor changes

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ function func() {
     document.getElementById("omg").classList.remove('active');
     }
     else {
-    document.getElementById("omg").classList.add('active');
+        document.getElementById("omg2").classList.remove('active');
+        setTimeout(() => {  document.getElementById("omg").classList.add('active'); }, 200);
+        
+    }
+    
+}
+function func2() {
+    var ok = window.getComputedStyle(document.getElementById("omg2"), null).display;
+    if(document.getElementById("omg2").classList.contains('active')) {
+    document.getElementById("omg2").classList.remove('active');
+    }
+    else {
+    document.getElementById("omg").classList.remove('active');
+    setTimeout(() => {      document.getElementById("omg2").classList.add('active');
+ }, 200);
     }
     
 }
