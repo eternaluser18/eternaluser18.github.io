b8dd575dcb84c321974d0f44b304e1e2bdff3784
eternaluser18 <80708910+eternaluser18@users.noreply.github.com>
2024-01-25 17:24:21 +0100

    cleaning
    cleaned names of variables. get code cleaner, more visible and readable

--- script.js
+++ script.js
@@ -1,200 +1,197 @@
-function func() {
-    var ok = window.getComputedStyle(document.getElementById("omg"), null).display;
-    if(document.getElementById("omg").classList.contains('active')) {
-    document.getElementById("omg").classList.remove('active');
+////////        MAIN FUNCTIONS          ///////////////            
+
+function change_visibility_aboutme() {
+    var ok = window.getComputedStyle(document.getElementById("ABOUTME_ID"), null).display;
+    if(document.getElementById("ABOUTME_ID").classList.contains('active')) {
+    document.getElementById("ABOUTME_ID").classList.remove('active');
     }
     else {
-        document.getElementById("omg2").classList.remove('active');
-        setTimeout(() => {  document.getElementById("omg").classList.add('active'); }, 200);
+        document.getElementById("PROJECTS_ID").classList.remove('active');
+        setTimeout(() => {  document.getElementById("ABOUTME_ID").classList.add('active'); }, 200);
         
     }
     
 }
 
-function show_snake() {
+function change_visibility_snake() {
     
-    var ok = window.getComputedStyle(document.getElementById("snake_w"), null).display;
-    if(document.getElementById("snake_w").classList.contains('active')) {
-    document.getElementById("snake_w").classList.remove('active');
+    var ok = window.getComputedStyle(document.getElementById("SNAKE_WINDOW"), null).display;
+    if(document.getElementById("SNAKE_WINDOW").classList.contains('active')) {
+    document.getElementById("SNAKE_WINDOW").classList.remove('active');
+    document.getElementsByTagName("body")[0].style = "overflow: visible";
     }
     else {
         //document.getElementById("omg2").classList.remove('active');
-        setTimeout(() => {  document.getElementById("snake_w").classList.add('active'); }, 200);
+        setTimeout(() => {  document.getElementById("SNAKE_WINDOW").classList.add('active'); }, 200);
+        setTimeout(() => {  document.getElementsByTagName("body")[0].style = "overflow: hidden"; }, 200);
+        
         
     }
     
 }
 
-function func2() {
-    var ok = window.getComputedStyle(document.getElementById("omg2"), null).display;
-    if(document.getElementById("omg2").classList.contains('active')) {
-    document.getElementById("omg2").classList.remove('active');
+function change_visibility_projects() {
+    var ok = window.getComputedStyle(document.getElementById("PROJECTS_ID"), null).display;
+    if(document.getElementById("PROJECTS_ID").classList.contains('active')) {
+    document.getElementById("PROJECTS_ID").classList.remove('active');
     }
     else {
-    document.getElementById("omg").classList.remove('active');
-    setTimeout(() => {      document.getElementById("omg2").classList.add('active');
+    document.getElementById("ABOUTME_ID").classList.remove('active');
+    setTimeout(() => {      document.getElementById("PROJECTS_ID").classList.add('active');
  }, 200);
     }
     
 }
 
 
-//////////////////////////////////////////SNAKE////////////////////////
+////////        SNAKE             /////////////// 
 
 //board
-var blockSize = 20;
-var rows = 20;
-var cols = 20;
-var board;
-var context;
-var points = -1;
+
+var SNAKE_blockSize = 20;
+var SNAKE_rows = 20;
+var SNAKE_cols = 20;
+var SNAKE_board;
+var SNAKE_context;
+var SNAKE_points = -1;
 
 //snake
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
-var velocityX = 0;
-var velocityY = 0;
 
-var snakeBody = [];
+var SNAKE_X = SNAKE_blockSize * 5;
+var SNAKE_Y = SNAKE_blockSize * 5;
+var SNAKE_velocityX = 0;
+var SNAKE_velocityY = 0;
+var SNAKE_Body = [];
 
-//food
+
 
 
 
 
 window.onload = function(){
-    board = document.getElementById("board");
-    board.height = rows * blockSize;
-    board.width = cols * blockSize;
-    context = board.getContext("2d");
+    SNAKE_board = document.getElementById("SNAKE_BOARD");
+    SNAKE_board.height = SNAKE_rows * SNAKE_blockSize;
+    SNAKE_board.width = SNAKE_cols * SNAKE_blockSize;
+    SNAKE_context = SNAKE_board.getContext("2d");
     
     
-    placeFood();
+    SNAKE_placeFood();
     
-    document.addEventListener("keyup", changeDirection);
+    document.addEventListener("keyup", SNAKE_changeDirection);
     
-    setInterval(update, 1000/5);
+    setInterval(SNAKE_update, 1000/5);
 }
 
-function update() {
+function SNAKE_update() {
     
-    context.fillStyle = "black";
-    context.fillRect(0, 0, board.width, board.height);
+    SNAKE_context.fillStyle = "black";
+    SNAKE_context.fillRect(0, 0, SNAKE_board.width, SNAKE_board.height);
     
     
-    if(snakeX >= 20 * blockSize){
-        restart();
+    if(SNAKE_X >= 20 * SNAKE_blockSize){
+        SNAKE_restart();
         window.alert("game over");
     }
-    if(snakeY >= 20 * blockSize){
-        restart();
+    if(SNAKE_Y >= 20 * SNAKE_blockSize){
+        SNAKE_restart();
         window.alert("game over");
     }
-    if(snakeX <= -1 * blockSize){
-        restart();
+    if(SNAKE_X <= -1 * SNAKE_blockSize){
+        SNAKE_restart();
         window.alert("game over");
     }
-    if(snakeY <= -1 * blockSize){
-        restart();
+    if(SNAKE_Y <= -1 * SNAKE_blockSize){
+        SNAKE_restart();
         window.alert("game over");
     }
     
-    context.fillStyle = "red";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
+    SNAKE_context.fillStyle = "red";
+    SNAKE_context.fillRect(foodX, foodY, SNAKE_blockSize, SNAKE_blockSize);
     
-    if(snakeX == foodX && snakeY == foodY){
-        snakeBody.push([foodX, foodY]);
+    if(SNAKE_X == foodX && SNAKE_Y == foodY){
+        SNAKE_Body.push([foodX, foodY]);
         
-        placeFood();
-        
+        SNAKE_placeFood();
     }
     
-    for (let i = snakeBody.length-1; i > 0; i--){
-        snakeBody[i] = snakeBody[i-1];
+    for (let i = SNAKE_Body.length-1; i > 0; i--){
+        SNAKE_Body[i] = SNAKE_Body[i-1];
     }
-    if(snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
+    if(SNAKE_Body.length) {
+        SNAKE_Body[0] = [SNAKE_X, SNAKE_Y];
     }
     
-    context.fillStyle="lime";
-    snakeX +=velocityX * blockSize;
-    snakeY +=velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-    for ( let i = 0; i < snakeBody.length; i++) {
-        if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
-            restart();
+    SNAKE_context.fillStyle="lime";
+    SNAKE_X +=SNAKE_velocityX * SNAKE_blockSize;
+    SNAKE_Y +=SNAKE_velocityY * SNAKE_blockSize;
+    SNAKE_context.fillRect(SNAKE_X, SNAKE_Y, SNAKE_blockSize, SNAKE_blockSize);
+    
+    for ( let i = 0; i < SNAKE_Body.length; i++) {
+        if(SNAKE_X == SNAKE_Body[i][0] && SNAKE_Y == SNAKE_Body[i][1]){
+            SNAKE_restart();
             window.alert("game over");
         }
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+        context.fillRect(SNAKE_Body[i][0], SNAKE_Body[i][1], SNAKE_blockSize, SNAKE_blockSize);
     }
     
     
     
 }
 
-function restart(){
-    velocityX = 0;
-    velocityY = 0;
-    snakeBody = [];
-    snakeX = 5 * blockSize;
-    snakeY = 5 * blockSize;
-    placeFood();
-    points = 0;
-    
+function SNAKE_restart(){
+    SNAKE_velocityX = 0;
+    SNAKE_velocityY = 0;
+    SNAKE_Body = [];
+    SNAKE_X = 5 * SNAKE_blockSize;
+    SNAKE_Y = 5 * SNAKE_blockSize;
+    SNAKE_placeFood();
+    SNAKE_points = 0;
+    document.getElementById("Points").innerHTML = SNAKE_points;
 }
 
-function changeDirection(e) {
-    if(e.code == "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
+function SNAKE_changeDirection(e) {
+    if(e.code == "ArrowUp" && SNAKE_velocityY != 1) {
+        SNAKE_velocityX = 0;
+        SNAKE_velocityY = -1;
     }
-    else if(e.code == "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
+    else if(e.code == "ArrowDown" && SNAKE_velocityY != -1) {
+        SNAKE_velocityX = 0;
+        SNAKE_velocityY = 1;
     }
-    else if(e.code == "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
+    else if(e.code == "ArrowLeft" && SNAKE_velocityX != 1) {
+        SNAKE_velocityX = -1;
+        SNAKE_velocityY = 0;
     }
-    else if(e.code == "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
+    else if(e.code == "ArrowRight" && SNAKE_velocityX != -1) {
+        SNAKE_velocityX = 1;
+        SNAKE_velocityY = 0;
     }
     else if(e.code == "Space") {
-        velocityX = 0;
-        velocityY = 0;
+        SNAKE_velocityX = 0;
+        SNAKE_velocityY = 0;
     }
 }
 
-function placeFood() {
+function SNAKE_placeFood() {
+    var pair = SNAKE_getRandomPairsExcluding(SNAKE_Body);
     
-    var pair = getRandomPairsExcluding(snakeBody);
     foodX = pair[0];
     foodY = pair[1];
         
-    
-    points++;
-    document.getElementById("Points").innerHTML = points;
-
-    
+    SNAKE_points++;
+    document.getElementById("Points").innerHTML = SNAKE_points;
 }
 
-function getRandomPairsExcluding(excludedPairs) {
-  
+function SNAKE_getRandomPairsExcluding(excludedPairs) {
+    function isPairExcluded(pair) {
+        return excludedPairs.some(excludedPair => (excludedPair[0] === pair[0] && excludedPair[1] === pair[1]));
+    } 
 
-  function isPairExcluded(pair) {
-    return excludedPairs.some(excludedPair =>
-      (excludedPair[0] === pair[0] && excludedPair[1] === pair[1])
-    );
-  }
+    while (1) {
+        var pair = [Math.floor(Math.random()* SNAKE_cols) * SNAKE_blockSize, Math.floor(Math.random() * SNAKE_rows) * SNAKE_blockSize];
 
-  while (1) {
-    var pair = [Math.floor(Math.random()* cols) * blockSize, Math.floor(Math.random() * rows) * blockSize];
-
-    if (!isPairExcluded(pair)) {
-      return pair;
-    }
-  }
-
-  
+        if (!isPairExcluded(pair)) {
+            return pair;
+        }
+    } 
 }
